76e2c4de1aa9dd8a97efb512d8edf1cf87adeebe
LKosharnaya <llyashkova@gmail.com>
2020-09-14 12:06:11 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $('.block-steps .row').on('click', function() {
+    $('.block-steps .block-form').removeClass('anime2');
     $('.block-steps .block-form').addClass('anime');
     $('.block-steps .row').addClass('anime');
 });
@@ -23,5 +24,30 @@ $('input.file').on('change', function() {
         $('.name-file').text(val);
         $('.name-file').show();
         $('.btn.file').hide();
+    }
+});
+$('.block-reviews-contact .block-contact').on('mouseover', function(){
+    $('.block-reviews-contact .block-rewiew').addClass('anime');
+
+});
+$('.block-reviews-contact .block-contact').on('mouseout', function(){
+    $('.block-reviews-contact .block-rewiew').removeClass('anime');
+
+});
+$(document).ready(function(){
+    var vwidth = $(window).width();
+    var $element = $('.block-licenses');   
+    if(vwidth<=1024){
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop() + $(window).height();
+            //Если скролл до конца елемента
+            var offset = $element.offset().top + $element.height()-100;
+            //Если скролл до начала елемента
+            //var offset = $element.offset().top;
+           
+            if (scroll > offset) {
+                $('.block-licenses .row').addClass('anime');
+            }
+         });
     }
 });
